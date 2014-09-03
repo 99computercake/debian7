10d033167284971f3509e617ec22caadaec969cf
Yuri Bhuana <youree82@gmail.com>
2014-09-03 16:50:39 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -5,10 +5,10 @@ export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
 MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
-ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
-if [ "$ether" = "" ]; then
-        ether=eth0
-fi
+#ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
+#if [ "$ether" = "" ]; then
+#        ether=eth0
+#fi
 
 # go to root
 cd
