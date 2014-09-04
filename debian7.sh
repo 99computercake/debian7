352650c830a354570937664d8745e05f20194af2
Yuri Bhuana <youree82@gmail.com>
2014-09-04 12:13:17 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -11,6 +11,12 @@ MYIP2="s/xxxxxxxxx/$MYIP/g";
 #        ether=eth0
 #fi
 
+#MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
+#if [ "$MYIP" = "" ]; then
+#		MYIP=$(wget -qO- ipv4.icanhazip.com)
+#fi
+#MYIP2="s/xxxxxxxxx/$MYIP/g";
+
 # go to root
 cd
 
