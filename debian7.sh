9d33ba0abad00f27fd8f5bf5afaad7f2d470cc01
99computercake <yuda.kaskus@gmail.com>
2014-11-13 18:56:00 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -90,34 +90,6 @@ sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php
 service php5-fpm restart
 service nginx restart
 
-# install openvpn
-wget -O /etc/openvpn/openvpn.tar "https://www.dropbox.com/s/1bgtsp34vpb4psq/openvpn-debian.tar?dl=0"
-cd /etc/openvpn/
-tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/youree82/debian7/master/1194.conf"
-service openvpn restart
-sysctl -w net.ipv4.ip_forward=1
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.up.rules "https://raw.github.com/youree82/debian7/master/iptables.up.rules"
-sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-sed -i $MYIP2 /etc/iptables.up.rules;
-iptables-restore < /etc/iptables.up.rules
-service openvpn restart
-
-# configure openvpn client config
-cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/youree82/debian7/master/1194-client.conf"
-sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
-PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
-useradd -M -s /bin/false youree82
-echo "youree82:$PASS" | chpasswd
-#echo "username" >> pass.txt
-#echo "password" >> pass.txt
-#tar cf client.tar 1194-client.ovpn pass.txt
-#cp client.tar /home/vps/public_html/
-cp 1194-client.ovpn client.ovpn
-cp client.ovpn /home/vps/public_html/
-cd
 
 # install badvpn
 wget -O /usr/bin/badvpn-udpgw "https://www.dropbox.com/s/hh1gviri22y070t/badvpn-udpgw?dl=0"
@@ -248,7 +220,6 @@ service cron restart
 service nginx start
 service php-fpm start
 service vnstat restart
-service openvpn restart
 service snmpd restart
 service ssh restart
 service dropbear restart
